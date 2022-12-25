1e22558082bd246ffc5025a641c24608467d0af0
PRACHI <prachiadhalage@gmail.com>
2022-12-25 19:31:18 +0530

    deletion

--- linkedlist/singlyLL.cpp
+++ linkedlist/singlyLL.cpp
@@ -19,11 +19,13 @@ class node{
     
         n->next= head;
         head = n ;
+        return;
     }
     //Insertion at Tail :
     void insertattail(node *&head , int val){
         node *n = new node(val);
-        if(head == NULL){
+        if(head == NULL)
+        {
             head = n ;
             return;
         }
@@ -47,6 +49,44 @@ class node{
         temp->next = n ;
     }
 
+    //Deletion at head :
+    void deleteAthead(node *&head){
+        
+        
+        node *temp = head;
+        head=temp->next;
+        temp->next=NULL;
+        delete (temp);
+        return;
+    }
+
+    //Deletion at Tail:
+    void deleteAttail(node *&head ){
+        node *temp = head;
+        node *temp1;
+        while(temp->next != NULL){
+            temp1 = temp;
+            temp = temp->next;
+        }
+        temp1->next = NULL;
+        delete (temp);
+        return;
+    }
+
+    //Deletion at any point :
+    void deleteatposition(node *&head ,  int pos){
+        node *temp = head;
+        node *temp1;
+        for(int i = 1; i <= pos ; i++){
+            temp1 = temp;
+            temp = temp->next;
+         
+        }
+        temp1->next = temp->next;
+        delete (temp);
+        return;
+    }  
+
     void display(node *&head){
         node *temp = head;
         while(temp != NULL){
@@ -55,6 +95,9 @@ class node{
         }
         cout<<"NULL"<<endl;
     }
+
+
+
 
 
 int main(){
@@ -70,8 +113,11 @@ int main(){
        cout<<"1.Insert at head:"<<endl;
        cout<<"2.Insert at tail:"<<endl;
        cout<<"3.insert at any position :"<<endl;
-       cout<<"4.Display the linkedlist:"<<endl;
-       cout<<"5.Enter the choice you want to perform:"<<endl;
+       cout<<"4.Delete at head :"<<endl;
+       cout<<"5.Delete at tail:"<<endl;
+       cout<<"6:Delete at any position:"<<endl;
+       cout<<"7.Display the linkedlist:"<<endl;
+       cout<<"8.Enter the choice you want to perform:"<<endl;
        cin>>c;
        cout<<endl;
 
@@ -93,7 +139,22 @@ int main(){
                 cin>>ipos;
                 insertatposition(head, ival,ipos); 
         break; 
-        case 4: display(head);
+        case 4:
+            deleteAthead(head);
+            cout<<"Node at head deleted successfully.."<<endl;
+        break;
+
+        case 5:
+            deleteAttail(head );
+            cout<<"Node at tail deleted successfully.."<<endl;
+        break;
+        case 6: 
+                cout<<"enter at which position you want to delete the value:"<<endl;
+                cin>>ipos;
+                deleteatposition(head, ipos);         
+        break;
+        
+        case 7: display(head);
         break;
        default:cout<<"entered the wrong choice :"<<endl;
         break;
